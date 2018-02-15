65884ab59d114a46a5c4a7e188204c2c7d8f2620
Jerson Martinez <sidemasterdelfuturo@gmail.com>
2018-02-14 21:41:01 -0600

    It's not TCB, else GNet in the licence

--- app/config/install/view/ic.InstallDesign.php
+++ app/config/install/view/ic.InstallDesign.php
@@ -116,7 +116,7 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" title="Licencia TheCodeBrain / InterCloud" id="myModalLabel">Terminos de licencia / TCB InterCloud</h4>
+        <h4 class="modal-title" title="Licencia de GNet" id="myModalLabel">Terminos de licencia | GNet (Gestión de Red)</h4>
       </div>
       <div class="modal-body">
         <p><b>Algún título por aquí de prueba</b></p>
