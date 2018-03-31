1ff80efbbe9d7541e588b6f67c010927709b4a90
Jerson Martinez <sidemasterdelfuturo@gmail.com>
2018-03-31 08:47:10 -0600

    Code PHP that get the information

---
+++ app/Desktop/Root/php/gn.AddDevice.php
@@ -0,0 +1,33 @@
+<?php
+    @session_start();
+
+    #Importar constantes.
+    include (@$_SESSION['getConsts']);
+
+    include (PF_CONNECT_SERVER);
+    include (PD_DESKTOP_ROOT_PHP."/gn.ssh.class.php");
+
+    $CN = new ConnectSSH();
+    $CN->ConnectDB($H, $U, $P, $D, $X);
+
+    #Recibir datos
+
+    $ADM_WhoIs = $_POST['WhoIs'];
+
+    if ($ADM_WhoIs == "ADM_Host"){
+    	$AliasHost 	= $_POST['AliasHost'];
+    	$IPNet 		= $_POST['IPNet'];
+    	$IPHost 	= $_POST['IPHost'];
+
+	    if (!$CN->checkNetwork($IPNet))
+	    	$CN->addNetwork($IPNet);
+
+	    if (!$CN->checkHost($IPHost))
+	    	$CN->addHost($IPNet, $IPHost, 0, "-", $AliasHost);
+
+    } else if ($ADM_WhoIs == "ADM_Server"){
+    	//
+    } else if ($ADM_WhoIs == "ADM_Router"){
+    	//
+    }
+?>
