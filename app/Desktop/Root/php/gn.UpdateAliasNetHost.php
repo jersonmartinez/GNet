9eddef522411575d557bb8baee4e8fedc87fd308
Jerson Martinez <sidemasterdelfuturo@gmail.com>
2018-04-05 18:43:01 -0600

    Algorithm to add an alias

--- app/Desktop/Root/php/gn.UpdateAliasNetHost.php
+++ app/Desktop/Root/php/gn.UpdateAliasNetHost.php
@@ -11,8 +11,31 @@
     $CN->ConnectDB($H, $U, $P, $D, $X);
 
     #Recibir datos
+    $ip_addr = $_POST['ip_addr'];
+    $alias   = $_POST['alias'];
+    
+    #Traer todos las coincidencias del supuesto enrutador
+    $getHosts = $CN->getAllHost();
 
-    $CN->updateNetworkAlias($_POST['ip_addr'], $_POST['alias']);
-    $CN->updateHostAlias($_POST['ip_addr'], $_POST['alias']);
-    echo "Ok";
+    if ($getHosts->num_rows > 0){
+        while ($listHosts = $getHosts->fetch_array(MYSQLI_ASSOC)){
+            if ($listHosts['ip_host'] == $ip_addr){
+                if ((bool)$listHosts['router']){
+
+                    #Cambiar todos los que son enrutadores
+                    if ($CN->updateHostRouterAlias($listHosts['ip_net'], $alias))
+                        echo "Ok";
+                    
+                    // echo $listHosts['ip_net']." | ".$listHosts['ip_host']." | ".$listHosts['router'];
+                } else {
+                    if ($CN->updateHostAlias($ip_addr, $alias))
+                        echo "Ok";
+                }
+
+            } else if ($listHosts['net_next'] == $ip_test){
+                if ($CN->updateNetworkAlias($ip_addr, $alias))
+                    echo "Ok";
+            }
+        }
+    }
 ?>
