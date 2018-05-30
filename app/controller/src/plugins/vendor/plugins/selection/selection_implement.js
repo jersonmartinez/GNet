5871733b8253c4513b211fd476245177670742dd
Jerson Martinez <sidemasterdelfuturo@gmail.com>
2018-05-30 12:33:51 -0600

    Resources about selection area of the devices management zone

---
+++ app/controller/src/plugins/vendor/plugins/selection/selection_implement.js
@@ -0,0 +1,40 @@
+const options = {
+
+    // Class for the selection-area
+    class: 'selection',
+    
+    // All elements in this container can be selected
+    containers: ['.box-wrap'],
+
+    // The container is also the boundary in this case
+    boundarys: ['.box-wrap'],
+
+    onMove(evt) {
+
+        // Get the currently selected elements and those
+        // which where removed since the last selection.
+        const selectedElements = evt.selectedElements;
+        const removedElements = evt.changedElements.removed;
+
+        // Add a custom class to the elements which where selected.
+        for (let se of selectedElements) {
+            se.classList.add('selected');
+        }
+
+        // Remove the class from elements which where removed 
+        // since the last selection.
+        for (let rm of removedElements) {
+            rm.classList.remove('selected');
+        }
+    },
+
+    onStop(evt) {
+
+        // Clear selection
+        for (let rm of evt.selectedElements) {
+            rm.classList.remove('selected');
+        }
+    },
+};
+
+Selection.create(options);
