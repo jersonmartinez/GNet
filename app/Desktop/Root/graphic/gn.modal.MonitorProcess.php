6ffb36386cf6ebb8629e83713ac86976c1cdc7e6
Jerson Martinez <jersonmartinezsm@gmail.com>
2018-12-09 11:30:53 -0600

    Adding three new files about Monitor Network, Processes and Properties of the host

---
+++ app/Desktop/Root/graphic/gn.modal.MonitorProcess.php
@@ -0,0 +1,40 @@
+<button type="hidden" class="ModalMonitorProcess" data-toggle="modal" data-target="#NowModalMonitorProcess"></button>
+
+<!-- <!- Modal -->
+<div class="modal fade" id="NowModalMonitorProcess" tabindex="-1" role="dialog" aria-labelledby="ModalMonitorProcess" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="ModalMonitorProcess"><span class="fa fa-keyboard-o"></span> Monitorización - Procesos</h4>
+            </div>
+
+            <div class="modal-body">        
+                <!-- Modal Monitor -->
+                <!-- <form id="Form_ModalMonitor"> -->
+                    <div class="row">
+                        <!-- Create Column with required .admin-grid class -->
+                        <div class="col-md-12">
+                            <div class="panel-heading">
+                                
+                            </div>
+
+                            <div class="panel-body AdminPanel_ResourcesMonitorProcess_PanelBodyModal">
+                                <!-- El contenido -->
+                                <br/>
+                            </div>
+
+                        </div>
+                        <!-- End Column -->
+                    </div>
+                <!-- </form> -->
+                <br>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" id="BtnCloseModalMonitorProcess" data-dismiss="modal">Cerrar</button>
+                <!-- <button type="button" class="btn btn-default btn-primary" id="Btn_ModalMonitorSave">Agregar</button> -->
+            </div>
+        </div>
+    </div>
+</div>
---
+++ app/Desktop/Root/graphic/gn.modal.MonitorProperties.php
@@ -0,0 +1,40 @@
+<button type="hidden" class="ModalMonitorProperties" data-toggle="modal" data-target="#NowModalMonitorProperties"></button>
+
+<!-- <!- Modal -->
+<div class="modal fade" id="NowModalMonitorProperties" tabindex="-1" role="dialog" aria-labelledby="ModalMonitorProperties" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="ModalMonitorProperties"><span class="fa fa-keyboard-o"></span> Monitorización - Propiedades</h4>
+            </div>
+
+            <div class="modal-body">        
+                <!-- Modal Monitor -->
+                <!-- <form id="Form_ModalMonitor"> -->
+                    <div class="row">
+                        <!-- Create Column with required .admin-grid class -->
+                        <div class="col-md-12">
+                            <div class="panel-heading">
+                                
+                            </div>
+
+                            <div class="panel-body AdminPanel_ResourcesMonitorProperties_PanelBodyModal">
+                                <!-- El contenido -->
+                                <br/>
+                            </div>
+
+                        </div>
+                        <!-- End Column -->
+                    </div>
+                <!-- </form> -->
+                <br>
+            </div>
+
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" id="BtnCloseModalMonitorProperties" data-dismiss="modal">Cerrar</button>
+                <!-- <button type="button" class="btn btn-default btn-primary" id="Btn_ModalMonitorSave">Agregar</button> -->
+            </div>
+        </div>
+    </div>
+</div>
