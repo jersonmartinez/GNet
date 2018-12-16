530e5cf540b0b98d551651c4fa09e51112ee88e0
Jerson Martinez <jersonmartinezsm@gmail.com>
2018-12-15 21:37:58 -0600

    Improving small details about Only Monitoring

--- app/Desktop/Root/graphic/gn.OnlyMonitoring.php
+++ app/Desktop/Root/graphic/gn.OnlyMonitoring.php
@@ -79,6 +79,94 @@
     }
 ?>
 
+<div class="row">
+    <!-- Create Column with required .admin-grid class -->
+    <div class="col-md-4 admin-grid">
+        <!-- Create Panel with required unique ID -->
+        <div class="panel panel-dark">
+            <div class="panel-heading">
+                <span class="fa fa-users"></span>
+                <span class="panel-title">Dirección MAC</span>
+            </div>
+            <div class="panel-body">
+                <table class="table">
+                    <?php
+                        if (!empty($MacAddress)){
+                            ?>
+                                <tr><td><?php echo $MacAddress; ?></td></tr>
+                            <?php
+                        } else {
+                            ?>
+                                <tr><td style="text-align: center;">Información no disponible</td></tr>
+                            <?php
+                        }
+                    ?>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 admin-grid">
+        <!-- Create Panel with required unique ID -->
+        <div class="panel panel-dark">
+            <div class="panel-heading">
+                <span class="fa fa-users"></span>
+                <span class="panel-title">Sistema Operativo</span>
+            </div>
+            <div class="panel-body">
+                <table class="table">
+                    <?php
+                        if (!empty($OSLinux)){
+                            ?>
+                                <tr><td>Linux</td></tr>
+                            <?php
+                        } else if (!empty($OSWindows)){
+                            ?>
+                                <tr><td>Windows</td></tr>
+                            <?php
+                        } else if (!empty($OSAndroid)){
+                            ?>
+                                <tr><td>Android</td></tr>
+                            <?php
+                        } else {
+                            ?>
+                                <tr><td style="text-align: center;">Información no disponible</td></tr>
+                            <?php
+                        }
+                    ?>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-4 admin-grid">
+        <!-- Create Panel with required unique ID -->
+        <div class="panel panel-dark">
+            <div class="panel-heading">
+                <span class="fa fa-users"></span>
+                <span class="panel-title">Estado del dispositivo</span>
+            </div>
+            <div class="panel-body">
+                <table class="table">
+                    <?php
+
+                        if (empty($HostUpOrDown)){
+                            ?>
+                                <tr><td>Encendido</td></tr>
+                            <?php
+                        } else {
+                            ?>
+                                <tr><td>Apagado</td></tr>
+                            <?php
+                        }
+                    ?>
+                </table>
+            </div>
+        </div>
+    </div>
+    <!-- End Column -->
+</div>
+
 <!-- Create Row -->
 <div class="row">
     <!-- Create Column with required .admin-grid class -->
@@ -90,13 +178,21 @@
                 <span class="panel-title">Inicialización</span>
             </div>
             <div class="panel-body">
-                <?php
-                    if (count($ArrayInitiating) > 0){
-                        foreach ($ArrayInitiating as $AInit){
-                            echo $AInit, "<br/>";
+                <table class="table">
+                    <?php
+                        if (count($ArrayInitiating) > 0){
+                            foreach ($ArrayInitiating as $AInit){
+                                ?>
+                                    <tr><td><?php echo $AInit; ?></td></tr>
+                                <?php
+                            }
+                        } else {
+                            ?>
+                                <tr><td style="text-align: center;">Información no disponible</td></tr>
+                            <?php
                         }
-                    }
-                ?>
+                    ?>
+                </table>
             </div>
         </div>
     </div>
@@ -109,13 +205,21 @@
                 <span class="panel-title">Completado</span>
             </div>
             <div class="panel-body">
-                <?php
-                    if (count($ArrayCompleted) > 0){
-                        foreach ($ArrayCompleted as $AComp){
-                            echo $AComp, "<br/>";
+                <table class="table">
+                    <?php
+                        if (count($ArrayCompleted) > 0){
+                            foreach ($ArrayCompleted as $AComp){
+                                ?>
+                                    <tr><td><?php echo $AComp; ?></td></tr>
+                                <?php
+                            }
+                        } else {
+                            ?>
+                                <tr><td style="text-align: center;">Información no disponible</td></tr>
+                            <?php
                         }
-                    }
-                ?>
+                    ?>
+                </table>
             </div>
         </div>
     </div>
@@ -134,13 +238,21 @@
                 <span class="panel-title">Puertos abiertos</span>
             </div>
             <div class="panel-body">
-                <?php
-                    if (count($ArrayPorts) > 0){
-                        foreach ($ArrayPorts as $APort){
-                            echo $APort, "<br/>";
+                <table class="table">
+                    <?php
+                        if (count($ArrayPorts) > 0){
+                            foreach ($ArrayPorts as $APort){
+                                ?>
+                                    <tr><td><?php echo $APort; ?></td></tr>
+                                <?php
+                            }
+                        } else {
+                            ?>
+                                <tr><td style="text-align: center;">Información no disponible</td></tr>
+                            <?php
                         }
-                    }
-                ?>
+                    ?>
+                </table>
             </div>
         </div>
     </div>
@@ -153,13 +265,21 @@
                 <span class="panel-title">Enumeración de Plataforma Común (CPE)</span>
             </div>
             <div class="panel-body">
-                <?php
-                    if (count($ArrayCPE) > 0){
-                        foreach ($ArrayCPE as $ACPE){
-                            echo $ACPE, "<br/>";
+                <table class="table">
+                    <?php
+                        if (count($ArrayCPE) > 0){
+                            foreach ($ArrayCPE as $ACPE){
+                                ?>
+                                    <tr><td><?php echo $ACPE; ?></td></tr>
+                                <?php
+                            }
+                        } else {
+                            ?>
+                                <tr><td style="text-align: center;">Información no disponible</td></tr>
+                            <?php
                         }
-                    }
-                ?>
+                    ?>
+                </table>
             </div>
         </div>
     </div>
@@ -178,13 +298,21 @@
                 <span class="panel-title">Aumento del retraso de envío</span>
             </div>
             <div class="panel-body">
-                <?php
-                   if (count($ArrayIncreasing) > 0){
-                        foreach ($ArrayIncreasing as $AIncre){
-                            echo $AIncre, "<br/>";
+                <table class="table">
+                    <?php
+                        if (count($ArrayIncreasing) > 0){
+                            foreach ($ArrayIncreasing as $AIncre){
+                                ?>
+                                    <tr><td><?php echo $AIncre; ?></td></tr>
+                                <?php
+                            }
+                        } else {
+                            ?>
+                                <tr><td style="text-align: center;">Información no disponible</td></tr>
+                            <?php
                         }
-                    }
-                ?>
+                    ?>
+                </table>
             </div>
         </div>
     </div>
@@ -202,15 +330,29 @@
                 <span class="panel-title">Paquetes transitados</span>
             </div>
             <div class="panel-body">
-                <?php
-                    if (!empty($RawPackets)){
-                        $ObtainSent = explode(":", explode("|", $RawPackets)[0])[1];
-                        echo "Enviado: ", $ObtainSent, "<br/>";
-                        
-                        $ObtainRcvd = explode(":", explode("|", $RawPackets)[1])[1];
-                        echo "Recibido: ", $ObtainRcvd, "<br/>";
-                    }
-                ?>
+                <table class="table">
+                    <tr>
+                        <th>Paquetes enviados</th>
+                        <th>Paquetes recibidos</th>
+                    </tr>
+                    <?php
+                        if (!empty($RawPackets)){
+                            $ObtainSent = explode(":", explode("|", $RawPackets)[0])[1];
+                            $ObtainRcvd = explode(":", explode("|", $RawPackets)[1])[1];
+
+                            ?>
+                                <tr>
+                                    <td><?php echo $ObtainSent; ?></td>
+                                    <td><?php echo $ObtainRcvd; ?></td>
+                                </tr>
+                            <?php
+                        } else {
+                            ?>
+                                <tr><td style="text-align: center;">Información no disponible</td></tr>
+                            <?php
+                        }    
+                    ?>
+                </table>
             </div>
         </div>
     </div>
@@ -223,74 +365,19 @@
                 <span class="panel-title">Saltos y tiempo de escaneo</span>
             </div>
             <div class="panel-body">
-                <?php
-                    if (!empty($TimeScanned)){
-                        echo $TimeScanned, "<br/>";
-                    }
-                ?>
-            </div>
-        </div>
-    </div>
-    <!-- End Column -->
-</div>
-<!-- End Row -->
-
-<!-- Create Row -->
-<div class="row">
-    <!-- Create Column with required .admin-grid class -->
-    <div class="col-md-4 admin-grid">
-        <!-- Create Panel with required unique ID -->
-        <div class="panel panel-dark">
-            <div class="panel-heading">
-                <span class="fa fa-users"></span>
-                <span class="panel-title">Dirección MAC</span>
-            </div>
-            <div class="panel-body">
-                <?php
-                     if (!empty($MacAddress)){
-                        echo $MacAddress, "<br/>";
-                    }
-                ?>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-4 admin-grid">
-        <!-- Create Panel with required unique ID -->
-        <div class="panel panel-dark">
-            <div class="panel-heading">
-                <span class="fa fa-users"></span>
-                <span class="panel-title">Sistema Operativo</span>
-            </div>
-            <div class="panel-body">
-                <?php
-                    if (!empty($OSLinux)){
-                        echo "Linux<br/>";
-                    } else if (!empty($OSWindows)){
-                        echo "Windows<br/>";
-                    } else if (!empty($OSAndroid)){
-                        echo "Android<br/>";
-                    }
-                ?>
-            </div>
-        </div>
-    </div>
-
-    <div class="col-md-4 admin-grid">
-        <!-- Create Panel with required unique ID -->
-        <div class="panel panel-dark">
-            <div class="panel-heading">
-                <span class="fa fa-users"></span>
-                <span class="panel-title">Estado del dispositivo</span>
-            </div>
-            <div class="panel-body">
-                <?php
-                    if (empty($HostUpOrDown)){
-                        echo "Encendido<br/>";
-                    } else {
-                        echo "Apagado<br/>";
-                    }
-                ?>
+                <table class="table">
+                    <?php
+                        if (!empty($TimeScanned)){
+                            ?>
+                                <tr><td><?php echo $TimeScanned; ?></td></tr>
+                            <?php
+                        } else {
+                            ?>
+                                <tr><td style="text-align: center;">Información no disponible</td></tr>
+                            <?php
+                        }
+                    ?>
+                </table>
             </div>
         </div>
     </div>
