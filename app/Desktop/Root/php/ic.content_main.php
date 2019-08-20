12288c7cbe9ac3e7381dc122064772383b4a051e
Frankenstein21 <frankierflores@gmail.com>
2019-08-19 23:36:21 -0600

    Doc. Instalación de rsyslog-mysql

--- app/Desktop/Root/php/ic.content_main.php
+++ app/Desktop/Root/php/ic.content_main.php
@@ -149,7 +149,7 @@
             <div class="col-md-12">
 
                 <!-- Create Panel with required unique ID -->
-                <div class="panel" id="pUniquewddd_TrlzdfdgackingNssedtworks">
+                <div class="panel" id="pUniquewddd_TlzdfdgackingNssedtworks">
                     <div class="panel-heading">
 
                         <span class="panel-icon"><i class="fa fa-sitemap"></i></span>
